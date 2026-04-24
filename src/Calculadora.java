f1e99daadd809ed22e6336eca427db1c2e55f5f1
Hector <hectorpallasvarona@gmail.com>
2026-04-24 20:37:50 +0200

    Implementacion dividir

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,5 +3,7 @@ public class Calculadora {
         System.out.println("Calculadora iniciada");
         Multiplicar multiplicar = new Multiplicar();
         System.out.println(multiplicar.calcular(5, 6));
+        Dividir dividir = new Dividir();
+        System.out.println(dividir.calcular(10, 2));
     }
 }
---
+++ src/Dividir.java
@@ -0,0 +1,5 @@
+public class Dividir {
+    public float calcular(int a,int b){
+        return a / b;
+    }
+}
