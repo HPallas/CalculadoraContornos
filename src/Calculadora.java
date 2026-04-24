0b2cde17193da5cdbf67431a53dfaf89c4bc7636
Hector <hectorpallasvarona@gmail.com>
2026-04-24 20:29:29 +0200

    Inclusion resta

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,5 +3,7 @@ public class Calculadora {
         System.out.println("Calculadora iniciada");
         Suma suma = new Suma();
         System.out.println(suma.calcular(2,3));
+        Resta resta = new Resta();
+        System.out.println(resta.calcular(2,3));
     }
 }
---
+++ src/Resta.java
@@ -0,0 +1,5 @@
+public class Resta {
+    public int calcular(int a, int b){
+        return a-b;
+    }
+}
