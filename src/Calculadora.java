126ad0ab4fb5c7a2298071fe59683eae698769a3
Hector <hectorpallasvarona@gmail.com>
2026-04-24 20:41:58 +0200

    .

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,6 +1,10 @@
 public class Calculadora {
     public static void main(String[] args) {
         System.out.println("Calculadora iniciada");
+        
+        
+        
+        
         Multiplicar multiplicar = new Multiplicar();
         System.out.println(multiplicar.calcular(5, 6));
         Dividir dividir = new Dividir();
